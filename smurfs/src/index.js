8683117fae771530b4df70dff9791bcfde269d09
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-10-11 10:31:48 -0700

    Stage 3

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,25 @@
+/* -----------------------------------------------------------------  */ 
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+/* -----------------------------------------------------------------  */ 
+/* I don't understand these imports below --------------------------  */ 
+/* -----------------------------------------------------------------  */ 
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { reducer } from ".//reducer/index";
+/* -----------------------------------------------------------------  */ 
+/* I don't understand these imports above --------------------------  */ 
+/* -----------------------------------------------------------------  */ 
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
+/* -----------------------------------------------------------------  */ 
